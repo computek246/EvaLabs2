709ecb947e293d56b59e3a490bb4cad24b004f91
walid wrdany <54823869+computek246@users.noreply.github.com>
2022-02-23 14:20:15 +0200

    walidwrdany_20220223_141954

--- EvaLabs.Security/EmailSettings.cs
+++ EvaLabs.Security/EmailSettings.cs
@@ -8,5 +8,6 @@
         public string Password { get; set; }
         public string Host { get; set; }
         public int Port { get; set; }
+        public bool EnableSsl { get; set; }
     }
 }
--- EvaLabs.Security/Implementations/EmailSender.cs
+++ EvaLabs.Security/Implementations/EmailSender.cs
@@ -1,21 +1,58 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace EvaLabs.Security.Implementations
 {
     public class EmailSender : IEmailSender
     {
+        private readonly ILogger<EmailSender> _logger;
         private readonly EmailSettings _options;
 
-        public EmailSender(IOptions<EmailSettings> options)
+        public EmailSender(IOptions<EmailSettings> options, ILogger<EmailSender> logger)
         {
+            _logger = logger;
             _options = options.Value;
         }
 
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            await Task.FromResult(true);
+
+            try
+            {
+                var fromAddress = new MailAddress(_options.FromEmail, _options.FromEmailName);
+                var toAddress = new MailAddress(email);
+
+                using var message = new MailMessage(fromAddress, toAddress)
+                {
+                    Subject = subject,
+                    Body = htmlMessage,
+                    IsBodyHtml = true
+                };
+
+                var smtp = new SmtpClient
+                {
+                    Credentials = new NetworkCredential(_options.FromEmail, _options.Password),
+                    Host = _options.Host,
+                    Port = _options.Port,
+                    EnableSsl = _options.EnableSsl,
+                    DeliveryMethod = SmtpDeliveryMethod.Network,
+                };
+
+                if (_options.EnableSendEmail)
+                {
+                    await smtp.SendMailAsync(message); 
+                }
+
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError(1, exception, exception.Message);
+            }
         }
     }
 }
